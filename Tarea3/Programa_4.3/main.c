8f7b62fac5d00aceed7f267c3fad505d8ff64a55
Marcos Terrero <Marcosterrerocastillo10@gmail.com>
2026-05-29 07:31:12 -0400

    Tarea3

---
+++ Tarea3/Programa_4.3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void f1(void);
+int K = 5;
+
+void main (void)
+{
+    int I;
+    for (I = 1; I <= 3; I++)
+        f1();
+}
+void f1(void)
+{
+    int K_local = 2;
+    K_local += K_local;
+    printf("\n\nEl valor de la variable local es: %d", K);
+    K = K + K_local;
+    printf("\nEl valor de la variable global es: %d", K);
+}
